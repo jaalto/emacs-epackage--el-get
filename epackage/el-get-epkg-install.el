f98f085c66ec4f24e58ff1b0b824fe3acd46292b
Jari Aalto <jari.aalto@cante.net>
2011-12-07 13:50:30 +0200

    epackage/el-get-epkg-install.el: (el-get-self-update): Disable

--- epackage/el-get-epkg-install.el
+++ epackage/el-get-epkg-install.el
@@ -5,9 +5,14 @@
 (autoload 'el-get-reload                        "el-get" "" t)
 (autoload 'el-get-update                        "el-get" "" t)
 (autoload 'el-get-update-all                    "el-get" "" t)
-(autoload 'el-get-self-update                   "el-get" "" t)
+;; (autoload 'el-get-self-update                   "el-get" "" t)
 (autoload 'el-get-remove                        "el-get" "" t)
 (autoload 'el-get-cd                            "el-get" "" t)
 (autoload 'el-get-make-recipes                  "el-get" "" t)
 (autoload 'el-get-checksum                      "el-get" "" t)
+
+(defun el-get-self-update ()
+  "Funtion el-get-self-update has been disabled. Use
+epackage.el interface to get updates.")
+
 (provide 'el-get-epkg-install)
