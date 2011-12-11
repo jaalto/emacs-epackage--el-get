4b4dbe4793c93acc3fcd5eb9e04d94ede2b76f58
Jari Aalto <jari.aalto@cante.net>
2011-12-11 02:59:26 +0200

    el-get-epkg-compile.el: rewrite

--- epackage/el-get-epkg-compile.el
+++ epackage/el-get-epkg-compile.el
@@ -1,6 +1,41 @@
 ;; byte-recompile-directory
 
-(dolist (file '("foo-lib.el" "foo.el"))
-        (byte-compile-file file))
+(dolist (file
+	 ;; order is important
+	 '("el-get-core.el"
+	   "el-get-autoloads.el"
+	   "el-get-custom.el"
+	   "el-get-dependencies.el"
+	   "el-get-byte-compile.el"
+	   "el-get-build.el"
+	   "el-get-install.el"
+	   "el-get-list-packages.el"
+	   "el-get-notify.el"
+	   "el-get-recipes.el"
+	   "el-get-status.el"
+	   ;;
+	   "methods/el-get-apt-get.el"
+	   "methods/el-get-brew.el"
+	   "methods/el-get-builtin.el"
+	   "methods/el-get-bzr.el"
+	   "methods/el-get-cvs.el"
+	   "methods/el-get-darcs.el"
+	   "methods/el-get-git.el"
+	   "methods/el-get-http.el"
+;;; requires 'package
+;;;	   "methods/el-get-elpa.el"
+	   "methods/el-get-emacsmirror.el"
+	   "methods/el-get-emacswiki.el"
+	   "methods/el-get-fink.el"
+	   "methods/el-get-git-svn.el"
+	   "methods/el-get-hg.el"
+	   "methods/el-get-http-tar.el"
+	   "methods/el-get-pacman.el"
+	   "methods/el-get-svn.el"
+	   ;; Requires plenty of above
+	   "el-get-methods.el"
+	   "el-get.el"))
+  (if (file-exists-p file)
+      (byte-compile-file file)))
 
 (provide 'el-get-epkg-compile)
