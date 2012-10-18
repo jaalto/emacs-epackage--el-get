194522cbb37137ff270bc3f984f774b0997a5b2d
Jari Aalto <jari.aalto@cante.net>
2012-10-18 18:09:27 +0300

    loaddefs: upstream/2012-10-15--git-97dd59e
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/el-get-epackage-0loaddefs.el
+++ epackage/el-get-epackage-0loaddefs.el
@@ -1,8 +1,38 @@
 
+;;;### (autoloads (el-get-emacswiki-refresh) "el-get-emacswiki"
+;;;;;;  "el-get-emacswiki.el" (20606 51619))
+;;; Generated autoloads from ../methods/el-get-emacswiki.el
+
+(autoload 'el-get-emacswiki-refresh "el-get-emacswiki" "\
+Generate recipes for all lisp files on Emacswiki.
+
+By default, this is done in a separate process so that you can
+continue to work while the recipes are being updated. If this
+fails, you can force the update to be done in-process by running
+this with a prefix arg (noninteractively: set optional arg
+`in-process' non-nil).
+
+\(fn &optional TARGET-DIR IN-PROCESS)" t nil)
+
+;;;***
+
+;;;### (autoloads (el-get-elpa-build-local-recipes) "el-get-elpa"
+;;;;;;  "el-get-elpa.el" (20606 51619))
+;;; Generated autoloads from ../methods/el-get-elpa.el
+
+(autoload 'el-get-elpa-build-local-recipes "el-get-elpa" "\
+retrieves list of ELPA packages and turn them to local recipe set.
+TARGET-DIR is the target directory
+DO-NOT-UPDATE will not update the package archive contents before running this.
+
+\(fn &optional TARGET-DIR DO-NOT-UPDATE)" t nil)
+
+;;;***
+
 ;;;### (autoloads (el-get-checksum el-get-make-recipes el-get-cd
-;;;;;;  el-get-self-update el-get-update-all el-get-version) "../el-get"
-;;;;;;  "../el-get.el" (20191 19775))
-;;; Generated autoloads from el-get.el
+;;;;;;  el-get-self-update el-get-update-all el-get-version) "el-get"
+;;;;;;  "el-get.el" (20606 51619))
+;;; Generated autoloads from ../el-get.el
 
 (autoload 'el-get-version "el-get" "\
 Message the current el-get version
@@ -12,7 +42,7 @@ Message the current el-get version
 (autoload 'el-get-update-all "el-get" "\
 Performs update of all installed packages.
 
-\(fn)" t nil)
+\(fn &optional NO-PROMPT)" t nil)
 
 (autoload 'el-get-cd "el-get" "\
 Open dired in the package directory.
@@ -28,13 +58,13 @@ entry which is not a symbol and is not already a known recipe.
 (autoload 'el-get-checksum "el-get" "\
 Compute the checksum of the given package, and put it in the kill-ring
 
-\(fn PACKAGE)" t nil)
+\(fn PACKAGE &optional PACKAGE-STATUS-ALIST)" t nil)
 
 ;;;***
 
 ;;;### (autoloads (el-get-list-packages) "el-get-list-packages"
-;;;;;;  "../el-get-list-packages.el" (20191 19775))
-;;; Generated autoloads from el-get-list-packages.el
+;;;;;;  "el-get-list-packages.el" (20606 51619))
+;;; Generated autoloads from ../el-get-list-packages.el
 
 (autoload 'el-get-list-packages "el-get-list-packages" "\
 Display a list of packages.
